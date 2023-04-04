4ecdfb1286fa490496b28c7824d3114cc0736613
Yash solanki <114523607+Ya30sh@users.noreply.github.com>
2023-04-04 17:26:07 +0530

    Add files via upload

---
+++ gym project-1/js/main.js
@@ -0,0 +1,199 @@
+/*  ---------------------------------------------------
+  Template Name: Gym
+  Description:  Gym Fitness HTML Template
+  Author: Colorlib
+  Author URI: https://colorlib.com
+  Version: 1.0
+  Created: Colorlib
+---------------------------------------------------------  */
+
+'use strict';
+
+(function ($) {
+
+    /*------------------
+        Preloader
+    --------------------*/
+    $(window).on('load', function () {
+        $(".loader").fadeOut();
+        $("#preloder").delay(200).fadeOut("slow");
+    });
+
+    /*------------------
+        Background Set
+    --------------------*/
+    $('.set-bg').each(function () {
+        var bg = $(this).data('setbg');
+        $(this).css('background-image', 'url(' + bg + ')');
+    });
+
+    //Canvas Menu
+    $(".canvas-open").on('click', function () {
+        $(".offcanvas-menu-wrapper").addClass("show-offcanvas-menu-wrapper");
+        $(".offcanvas-menu-overlay").addClass("active");
+    });
+
+    $(".canvas-close, .offcanvas-menu-overlay").on('click', function () {
+        $(".offcanvas-menu-wrapper").removeClass("show-offcanvas-menu-wrapper");
+        $(".offcanvas-menu-overlay").removeClass("active");
+    });
+
+    // Search model
+    $('.search-switch').on('click', function () {
+        $('.search-model').fadeIn(400);
+    });
+
+    $('.search-close-switch').on('click', function () {
+        $('.search-model').fadeOut(400, function () {
+            $('#search-input').val('');
+        });
+    });
+
+    //Masonary
+    $('.gallery').masonry({
+        itemSelector: '.gs-item',
+        columnWidth: '.grid-sizer',
+        gutter: 10
+    });
+
+    /*------------------
+		Navigation
+	--------------------*/
+    $(".mobile-menu").slicknav({
+        prependTo: '#mobile-menu-wrap',
+        allowParentLinks: true
+    });
+
+    /*------------------
+        Carousel Slider
+    --------------------*/
+    var hero_s = $(".hs-slider");
+    hero_s.owlCarousel({
+        loop: true,
+        margin: 0,
+        nav: true,
+        items: 1,
+        dots: false,
+        animateOut: 'fadeOut',
+        animateIn: 'fadeIn',
+        navText: ['<i class="fa fa-angle-left"></i>', '<i class="fa fa-angle-right"></i>'],
+        smartSpeed: 1200,
+        autoHeight: false,
+        autoplay: false
+    });
+
+    /*------------------
+        Team Slider
+    --------------------*/
+    $(".ts-slider").owlCarousel({
+        loop: true,
+        margin: 0,
+        items: 3,
+        dots: true,
+        dotsEach: 2,
+        smartSpeed: 1200,
+        autoHeight: false,
+        autoplay: true,
+        responsive: {
+            320: {
+                items: 1,
+            },
+            768: {
+                items: 2,
+            },
+            992: {
+                items: 3,
+            }
+        }
+    });
+
+    /*------------------
+        Testimonial Slider
+    --------------------*/
+    $(".ts_slider").owlCarousel({
+        loop: true,
+        margin: 0,
+        items: 1,
+        dots: false,
+        nav: true,
+        navText: ['<i class="fa fa-angle-left"></i>', '<i class="fa fa-angle-right"></i>'],
+        smartSpeed: 1200,
+        autoHeight: false,
+        autoplay: true
+    });
+
+    /*------------------
+        Image Popup
+    --------------------*/
+    $('.image-popup').magnificPopup({
+        type: 'image'
+    });
+
+    /*------------------
+        Video Popup
+    --------------------*/
+    $('.video-popup').magnificPopup({
+        type: 'iframe'
+    });
+
+    /*------------------
+        Barfiller
+    --------------------*/
+    $('#bar1').barfiller({
+        barColor: '#ffffff',
+        duration: 2000
+    });
+    $('#bar2').barfiller({
+        barColor: '#ffffff',
+        duration: 2000
+    });
+    $('#bar3').barfiller({
+        barColor: '#ffffff',
+        duration: 2000
+    });
+
+    function  computeBMI () {
+        // user inputs
+        var height = Number(document.getElementById("bmi-hight").value);
+        var weight = Number(document.getElementById("bmi-weight").value);
+        var result = weight / (height * height);
+
+        //Display result of calculation
+        var output = Math.round(result * 100) / 100;
+        if (output < 18.5) {
+            document.getElementById("bmi-result").value = output + " (Underweight)";
+        } else if (output >= 18.5 && output <= 25) {
+            document.getElementById("bmi-result").value = output + " (Normal)";
+        } else if (output >= 25 && output <= 30) {
+            document.getElementById("bmi-result").value = output + " (Obese)";
+        }else if (output > 30) {
+            document.getElementById("bmi-result").value = output + " (Overweight)";
+        } else {
+            document.getElementById("bmi-result").value = output; 
+        }
+    }
+
+    $('#bmi-submit').on('click', function(){
+        computeBMI();
+    });
+
+    $('.table-controls ul li').on('click', function () {
+        var tsfilter = $(this).data('tsfilter');
+        $('.table-controls ul li').removeClass('active');
+        $(this).addClass('active');
+
+        if (tsfilter == 'all') {
+            $('.class-timetable').removeClass('filtering');
+            $('.ts-meta').removeClass('show');
+        } else {
+            $('.class-timetable').addClass('filtering');
+        }
+        $('.ts-meta').each(function () {
+            $(this).removeClass('show');
+            if ($(this).data('tsmeta') == tsfilter) {
+                $(this).addClass('show');
+            }
+        });
+    });
+
+})(jQuery);
